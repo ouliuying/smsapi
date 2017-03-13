6fa46981e6b4d0cef2d4af1858762b9cad56a550
ouyang <ouyang2001@gmail.com>
2017-03-13 18:27:03 +0800

    更新接口连接
    更新接口连接

--- CPP短信接口SDK/win api/IkyunSmsSender/CApiTest/CApiTest.cpp
+++ CPP短信接口SDK/win api/IkyunSmsSender/CApiTest/CApiTest.cpp
@@ -15,7 +15,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	 每个用户的提交连接不同的，必须到自己帐号的后台【http://sms.ikyun.net/】
 	 【帐号中心】=>【我的信息】=>【Api接口信息】
 	*/
-	context->apiurl = L"http://sms.ikyun.net/abcapi/sms/ebpbcons";
+	context->apiurl = L"http://sms.ikyun.net/apiex/sms/ebpbcons";
 	context->appid = L"c0cb7b8ad8e64296a88c99d923345148";
 	context->appkey = L"c4d963e9158444b7b71e52fb205eb1c3";
 	TCHAR retmsg[2048];
--- CPP短信接口SDK/win api/IkyunSmsSender/CppTest/CppTest.cpp
+++ CPP短信接口SDK/win api/IkyunSmsSender/CppTest/CppTest.cpp
@@ -16,7 +16,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	每个用户的提交连接不同的，必须到自己帐号的后台【http://sms.ikyun.net/】
 	【帐号中心】=>【我的信息】=>【Api接口信息】
 	*/
-	IkyunApi::CIkyunSmsSender sender(L"http://sms.ikyun.net/abcapi/sms/ebpbcons", L"c0cb7b8ad8e64296a88c99d923345148", L"c4d963e9158444b7b71e52fb205eb1c3");
+	IkyunApi::CIkyunSmsSender sender(L"http://sms.ikyun.net/apiex/sms/ebpbcons", L"c0cb7b8ad8e64296a88c99d923345148", L"c4d963e9158444b7b71e52fb205eb1c3");
 	std::wstring msg = L"你好，中文!!";
 	std::wcout << L"你好，中文"<<std::endl;
 	std::wstring retmsg=sender.Send(L"186219915xx,186219915yy", L"88", msg);
