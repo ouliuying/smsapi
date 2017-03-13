94d52be10907c315523196d0bde0a0afd10a4ff8
ouyang <ouyang2001@gmail.com>
2017-03-13 19:08:08 +0800

    ''

--- python短信接口/python/test.py
+++ python短信接口/python/test.py
@@ -1,5 +1,5 @@
 import ikyunsender
-sender=ikyunsender.IkYunSender("http://sms.ikyun.net/abcapi/sms/ebpbcons","c0cb7b8ad8e64296a88c99d923345148","c4d963e9158444b7b71e52fb205eb1c3")
+sender=ikyunsender.IkYunSender("http://sms.ikyun.net/apiex/sms/ebpbcons","c0cb7b8ad8e64296a88c99d923345148","c4d963e9158444b7b71e52fb205eb1c3")
 msg="你好"
 retmsg=sender.send("186219915xx,186219915yy","88",msg)
 print(retmsg)
--- python短信接口/python27/test.py
+++ python短信接口/python27/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ikyunsender
-sender=ikyunsender.IkYunSender("http://sms.ikyun.net/abcapi/sms/ebpbcons","c0cb7b8ad8e64296a88c99d923345148","c4d963e9158444b7b71e52fb205eb1c3")
+sender=ikyunsender.IkYunSender("http://sms.ikyun.net/apiex/sms/ebpbcons","c0cb7b8ad8e64296a88c99d923345148","c4d963e9158444b7b71e52fb205eb1c3")
 msgs="你哄啊！！！"
 retmsg=sender.send("186219915xx,186219915yy","88",msg)
 print retmsg
