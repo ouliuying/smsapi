c8982236103956471914c63cbdabbfbe0ba8e190
ouyang <ouyang2001@gmail.com>
2017-04-01 15:29:04 +0800

    接口连接更新
    接口连接更新

--- PHP短信接口/test.php
+++ PHP短信接口/test.php
@@ -5,7 +5,7 @@ require_once('smssender.php');
 $sender=new iapi\SmsSender();
 $sender->setAppID("c0cb7b8ad8e64296a88c99d923345148");
 $sender->setAppKey("c4d963e9158444b7b71e52fb205eb1c3");
-$sender->setApiUrl("http://sms.ikyun.net/abcapi/sms/ebpbcons");//ebpbcons 必须被换掉 用户自己的标识
+$sender->setApiUrl("http://sms.ikyun.net/apiex/sms/ebpbcons");//ebpbcons 必须被换掉 用户自己的标识
 $msg=" 你好！！！";
 $ret=$sender->sendSms("186219915xx,186219915xx33",$msg,"88");
 if($ret===FALSE)
